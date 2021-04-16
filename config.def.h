51de885bd5d789bc3f3b22594390de557269e5e2
Kristaps Čivkulis <kristaps.civkulis@gmail.com>
2021-04-16 19:07:43 +0300

    Switch back to firefox

--- config.def.h
+++ config.def.h
@@ -34,9 +34,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Brave-browser",  NULL,       NULL,       1 << 4,       0,           -1 },
-	{ "r_x11",			NULL,		NULL,		0,			  1,		   -1 },
-	{ "R_x11",			NULL,		NULL,		0,			  1,		   -1 },
+	{ "firefox",  NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "r_x11",    NULL,	  NULL,	      0, 	    1,		 -1 },
+	{ "R_x11",    NULL,    	  NULL,	      0,      	    1,		 -1 },
 };
 
 /* layout(s) */
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *firefoxcmd[] = { "brave", NULL };
+static const char *firefoxcmd[] = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
